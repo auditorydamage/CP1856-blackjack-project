a2cfd375bded6ffefd05059bffba22d707e13fe0
auditorydamage <auditorydamage@gmail.com>
2025-11-25 18:17:59 -0330

    Create db.py

---
+++ db.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import csv
+
+def getMoney(amount, dbfile):
+    # Read the player's available money
+    return
+
+
+def putMoney(amount, dbfile):
+    # Update the player's wallet, or create a new one if none exists
+    return
